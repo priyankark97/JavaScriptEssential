d5602a555f41c618d21939deb707f93565fae5d8
priyankark97 <priyankaramakrishna97@gmail.com>
2025-03-04 11:42:57 -0500

    array itteration

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breafkfast = ['pancakes', 'eggs'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const bf = breafkfast.map((item, index) => `<p> Item ${index + 1} : ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = bf;
+
+ let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
